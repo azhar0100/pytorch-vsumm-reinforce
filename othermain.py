a34a41263d620be6bc6f841c1c6a03207be85229
azhar <azhartahir0100@gmail.com>
2019-08-04 01:45:39 +0500

    merged

--- othermain.py
+++ othermain.py
@@ -14,11 +14,7 @@ import torch.nn as nn
 import torch.backends.cudnn as cudnn
 from torch.optim import lr_scheduler
 from torch.distributions import Bernoulli
-<<<<<<< HEAD
 from ax import optimize
-=======
->>>>>>> 520fcb36a2f728bca78ddc5499d6308189258131
-
 import atexit
 
 from utils import Logger, read_json, write_json, save_checkpoint
